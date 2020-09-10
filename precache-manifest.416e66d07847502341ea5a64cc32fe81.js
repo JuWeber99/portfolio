a112d1f2102adafeb7e095d23507e4d50c106810
JuWeber99 <s_weberj@stud.hwr-berlin.de>
2020-09-10 03:37:14 +0200

    Updates

---
+++ precache-manifest.416e66d07847502341ea5a64cc32fe81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c349280dda6aa374c91f7bc2da81c41c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a905307d0716fc303741",
+    "url": "/portfolio/static/css/2.087a99ea.chunk.css"
+  },
+  {
+    "revision": "390152ba966a5a72dc85",
+    "url": "/portfolio/static/css/main.412f0fdc.chunk.css"
+  },
+  {
+    "revision": "a905307d0716fc303741",
+    "url": "/portfolio/static/js/2.38d56644.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.38d56644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390152ba966a5a72dc85",
+    "url": "/portfolio/static/js/main.49b9a6bb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5a4d983c0cbc27178490f356b375afe6.js"
+  "/portfolio/precache-manifest.416e66d07847502341ea5a64cc32fe81.js"
 );
 
 self.addEventListener('message', (event) => {
